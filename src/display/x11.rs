bab26ba1013d36daa3d12cef0cdf4e0d0a0d3f83
Hunter Leath <h@hunterleath.com>
2017-11-24 16:37:46 -0500

    Add ability to draw colors to X

--- src/display/x11.rs
+++ src/display/x11.rs
@@ -12,6 +12,7 @@ pub struct XScreen {
     win: u64,
     white: u64,
     black: u64,
+    cmap: Colormap,
 }
 
 impl XScreen {
@@ -31,11 +32,12 @@ impl XScreen {
         let screen = XDefaultScreen(display);
         let (black, white) = (XBlackPixel(display, screen),
                               XWhitePixel(display, screen));
+        let cmap = XDefaultColormap(display, screen);
         let win = XCreateSimpleWindow(display, XDefaultRootWindow(display),
                                       /*x=*/0, /*y=*/0, /*width=*/128, /*height=*/160,
                                       /*border_width=*/0, /*border=*/black, /*background=*/black);
-        let win_title = ffi::CString::new("Ajidamal Emulator").unwrap();
-        let win_icon = ffi::CString::new("aji/emu").unwrap();
+        let win_title = ffi::CString::new("Ajidamal Simulator").unwrap();
+        let win_icon = ffi::CString::new("aji/sim").unwrap();
         XSetStandardProperties(display, win, win_title.as_ptr(), win_icon.as_ptr(),
                                /*pixmap=*/x_none, /*argv=*/ptr::null_mut(), /*argc=*/0,
                                /*hints=*/ptr::null_mut());
@@ -47,7 +49,8 @@ impl XScreen {
             screen: screen,
             win: win,
             white: white,
-            black: black
+            black: black,
+            cmap: cmap
         };
 
         screen.flush();
@@ -61,11 +64,27 @@ impl Screen for XScreen {
         (128, 160)
     }
 
-    fn write_pixel(&mut self, x: usize, y: usize, _color: Color) {
+    fn write_pixel(&mut self, x: usize, y: usize, color: Color) {
+        let (r, g, b) = color.intensities();
+
         unsafe {
+            let mut xcolor = XColor {
+                pixel: 0,
+                // Convert 8-bit color to 16-bit color
+                red: (r << 8) as u16,
+                green: (g << 8) as u16,
+                blue: (b << 8) as u16,
+                flags: 0,
+                pad: 0,
+            };
+            let status = XAllocColor(self.display, self.cmap, &mut xcolor);
+            if status == 0 {
+                panic!("Color allocation failed.");
+            }
+
             let gc = XCreateGC(self.display, self.win, 0, ptr::null_mut());
             XSetBackground(self.display, gc, self.black);
-            XSetForeground(self.display, gc, self.white);
+            XSetForeground(self.display, gc, xcolor.pixel);
 
             XDrawPoint(self.display, self.win, gc, x as i32, y as i32);
             XFreeGC(self.display, gc);
